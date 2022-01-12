609df871a290c4570a16d0fce8b9fed9348226af
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-01-12 21:28:18 +0800

    refactor: switch to react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Button } from 'antd'
+
+const App = () => {
+  return (
+    <>
+      <Button type="primary">test</Button>
+    </>
+  )
+}
+
+export default App
--- src/components/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="main">
-    <router-link to="/about">Hello ALL!test</router-link>
-  </div>
-</template>
