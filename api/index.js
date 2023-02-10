198f71260258772ed006179f817949970fd6a2ed
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-02-11 00:30:15 +0800

    feat: update

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import dayjs from './utils/day.js'
 export default async (req, res) => {
   const data = {
     msg: 'hello world',
-    time: dayjs().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')
+    time: dayjs().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss'),
+    url: process.env.DATABASE_URL
   }
 
   res.status(200).json(data)
