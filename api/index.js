01ff4e470ddd37b8b4f09526d3f465cd88578082
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-07-24 21:20:19 +0800

    feat: update

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-// import prismaClient from './lib/prismaClient.js'
+import prismaClient from './lib/prismaClient.js'
 
 export default async(req, res) => {
-  // const User = await prismaClient.User.findMany()
+  const User = await prismaClient.User.findMany()
 
+  res.status(200).json({ msg: 'Hello', data: User, nodeEnv: process.env.NODE_ENV })
   // res.status(200).json({ msg: 'Hello', data: User, nodeEnv: process.env })
-  // res.status(200).json({ msg: 'Hello', data: User, nodeEnv: process.env })
-  res.status(200).json({ nodeEnv: process.env.NODE_ENV, databaseURL: process.env.DATABASE_URL })
+  // res.status(200).json({ nodeEnv: process.env.NODE_ENV, databaseURL: process.env.DATABASE_URL })
   console.log(process.env.NODE_ENV)
 }
