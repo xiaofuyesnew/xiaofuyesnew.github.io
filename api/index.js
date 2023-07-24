f8b49be8b2e14969a9ce4ea26c6f0e548b8989a4
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-07-24 22:41:24 +0800

    test: vercelignore

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import prismaClient from './lib/prismaClient.js'
 
 export default async(req, res) => {
   const User = await prismaClient.User.findMany()
-
+  console.log('test')
   res.status(200).json({ msg: 'Hello', data: User, nodeEnv: process.env.NODE_ENV })
   // res.status(200).json({ msg: 'Hello', data: User, nodeEnv: process.env })
   // res.status(200).json({ nodeEnv: process.env.NODE_ENV, databaseURL: process.env.DATABASE_URL })
