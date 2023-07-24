440b363949edfe566fb52d2f02574c9ee2b72c50
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-07-24 21:19:36 +0800

    feat: update

--- api/lib/prismaClient.js
+++ api/lib/prismaClient.js
@@ -2,6 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const prismaClient = global.prismaClient || new PrismaClient()
 
-if (process.env.NODE_ENV === 'development') global.prisma = prismaClient
+if (process.env.NODE_ENV === 'development') global.prismaClient = prismaClient
 
 export default prismaClient
