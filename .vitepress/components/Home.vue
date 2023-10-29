622b2be0698ca0ff5d4e2a65a77da0d665c6aaf2
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-10-29 21:55:19 +0800

    feat: home page edit

---
+++ .vitepress/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="post max-w-3xl mx-auto p-4">
+    <div class="flex justify-center py-10">
+      <img class="w-30 rounded-md" src="https://avatars.githubusercontent.com/u/8016493?v=4" alt="avatar">
+    </div>
+    <h1 class="text-3xl font-bold mb-5">Allen Wong</h1>
+    <p class="mb-5">Hi, I am Allen Wong, a full-stack open source coder.🧙‍♂️</p>
+    <p class="mb-5">MAY use <span class="text-[#3178C6]" i-simple-icons:typescript /> BUT a <span class="text-[#F7DF1E]" i-simple-icons:javascript /> advocate.</p>
+    <p>Find me on</p>
+    <p flex="~ gap-3 wrap" class="pt-2 mb-5">
+      <a href="https://github.com/xiaofuyesnew" target="_blank">
+        <span i-simple-icons:github /> GitHub
+      </a>
+      <a href="https://twitter.com/xiaofuyesnew" target="_blank">
+        <span i-simple-icons:twitter /> Twitter
+      </a>
+      <a href="https://discord.gg/Sr6gKR2y" target="_blank">
+        <span i-simple-icons:discord /> Discord Server
+      </a>
+      <a href="https://youtube.com/xiaofuyesnew" target="_blank">
+        <span i-simple-icons:youtube /> YouTube
+      </a>
+      <a href="https://instagram.com/xiaofuyesnew" target="_blank">
+        <span i-simple-icons:instagram /> Instagram
+      </a>
+      <a href="https://juejin.cn/user/2928754705564184" target="_blank">
+        <span i-simple-icons:juejin /> 稀土掘金 
+      </a>
+      <a href="https://www.zhihu.com/people/xiaofuyesnew" target="_blank">
+        <span i-simple-icons:zhihu /> 知乎
+      </a>
+      <a href="https://weibo.com/u/1183565322" target="_blank">
+        <span i-simple-icons:sinaweibo /> 微博
+      </a>
+      <a href="https://space.bilibili.com/747089" target="_blank">
+        <span i-simple-icons:bilibili /> 哔哩哔哩
+      </a>
+    </p>
+    <p>If you enjoy my works or any of my work helps you, consider <a href="https://www.buymeacoffee.com/xiaofuyesnew"
+        target="_blank"><span i-raphael-coffee /> buy me a coffee</a> or <a href="https://github.com/xiaofuyesnew"
+        target="_blank"><span i-carbon:lightning /> 爱发电</a> to encourage me to work more and better.🫰</p>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
